8c81bb32f5a6cfbbbfb850ee3e79bd34213003ed
Gabesz007 <szolgalati.torocsik@gmail.com>
2024-01-20 22:22:50 +0100

    Haromszog befejezve, kerulet kiszamitva

---
+++ Programozás/torocsik_gabor_haromszogjavitas.py
@@ -0,0 +1,17 @@
+# 2) Feladat – Írjon programot, amely kiszámolja egy háromszög kerületét.
+# Törőcsik Gábor 2023.01.17.
+
+print("Törőcsik Gábor 2023.01.17.\nEz a program kiszámolja egy adott háromszög kerületét a megadott oldalhosszok alapján.")
+
+a = float(input("Kerem, adja meg az 'a' oldal hosszat, szamokkal, majd nyomjon egy 'entert': "))
+b = float(input("Kerem, adja meg a 'b' oldal hosszat, szamokkal, majd nyomjon egy 'entert': "))
+c = float(input("Kerem, adja meg a 'c' oldal hosszat, szamokkal, majd nyomjon egy 'entert': "))
+K = a + b + c
+
+print(f"A háromszög kerülete: {K}!")
+
+
+    
+
+
+
